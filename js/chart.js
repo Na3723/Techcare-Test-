74cfa65e31a23b439b73e84f59e1d7ed6a8814f5
Nelly Arjarasumpun <na3723@nyu.edu>
2026-01-13 17:51:10 +0700

    z

--- js/chart.js
+++ js/chart.js
@@ -3,24 +3,47 @@ setTimeout(() => {
   const systolic = window.historyData.map(h => h.blood_pressure.systolic.value);
   const diastolic = window.historyData.map(h => h.blood_pressure.diastolic.value);
 
-  new Chart(document.getElementById("bpChart"), {
-    type: "line",
-    data: {
-      labels,
-      datasets: [
-        {
-          label: "Systolic",
-          data: systolic,
-          borderColor: "#ff6b9a",
-          tension: 0.4
-        },
-        {
-          label: "Diastolic",
-          data: diastolic,
-          borderColor: "#6c5ce7",
-          tension: 0.4
+ new Chart(document.getElementById("bpChart"), {
+  type: "line",
+  data: {
+    labels,
+    datasets: [
+      {
+        label: "Systolic",
+        data: systolic,
+        borderColor: "#ff6b9a",
+        backgroundColor: "#ff6b9a33",
+        tension: 0.4,
+      },
+      {
+        label: "Diastolic",
+        data: diastolic,
+        borderColor: "#6c5ce7",
+        backgroundColor: "#6c5ce733",
+        tension: 0.4,
+      }
+    ]
+  },
+  options: {
+    plugins: {
+      legend: {
+        position: 'right',
+        labels: {
+          usePointStyle: true,
+          padding: 20,
         }
-      ]
+      }
+    },
+    scales: {
+      x: {
+        ticks: {
+          maxRotation: 45,
+          minRotation: 45
+        }
+      },
+      y: {
+        beginAtZero: false
+      }
     }
-  });
-}, 500);
+  }
+});
