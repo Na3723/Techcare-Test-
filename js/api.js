b9f797aeeefbaec324570c56d6175ddf2bd47514
Nelly Arjarasumpun <na3723@nyu.edu>
2026-01-14 00:58:38 +0700

    2

--- js/api.js
+++ js/api.js
@@ -1,5 +1,20 @@
+const API_URL = "https://fedskillstest.coalitiontechnologies.workers.dev";
+
 async function fetchPatients() {
-  const res = await fetch("https://coalition-medical-data.vercel.app/data.json");
+  const username = "coalition";
+  const password = "skills-test";
+
+  const auth = btoa(`${username}:${password}`);
+
+  const res = await fetch(API_URL, {
+    headers: {
+      Authorization: `Basic ${auth}`
+    }
+  });
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch patient data");
+  }
+
   return res.json();
 }
-
